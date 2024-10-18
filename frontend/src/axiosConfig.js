7b429c7d226baa680778dc81fbb1e314914271de
jeel28 <jeelpatel0318@gmail.com>
2024-10-18 13:17:21 +0530

    fix axiosConfig

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
+// Set base URL based on the environment (development or production)
 axios.defaults.baseURL =
   process.env.NODE_ENV !== 'production'
-    ? 'http://localhost:5000'  // Local development
-    : 'https://food-mine.vercel.app/api';  // Vercel backend (make sure the path is correct)
+    ? 'http://localhost:5000'  // Local development backend
+    : 'https://food-mine.vercel.app';  // Correct production backend path
